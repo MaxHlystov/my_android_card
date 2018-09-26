c45fb242fd13e1751cc7a97b717f2c3b337c8d41
MaxHlystov <maxvls@gmail.com>
2018-09-27 01:28:13 +0300

    Check all errors, except #9 (custov view components)

---
+++ app/src/main/java/ru/fmtk/khlystov/myportfolio/IConfigValues.java
@@ -0,0 +1,10 @@
+package ru.fmtk.khlystov.myportfolio;
+
+
+public interface IConfigValues {
+    String getMyEmail();
+    String getURL_Telegram();
+    String getURL_Github();
+    String getURL_Linkedin();
+    String getURL_Stepik();
+}
